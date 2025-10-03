dc9f3b793920b660b342dcdea788c9f009078416
punksguitar <hanahanaogutaro@gmail.com>
2025-10-03 14:51:23 +0900

    [add]user生成時にshare_id自動生成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  before_create :generate_share_id
+
+  private
+
+  def generate_share_id
+    loop do
+      self.share_id = SecureRandom.hex(8)
+      break unless User.exists?(share_id: self.share_id)
+    end
+  end
+
 end

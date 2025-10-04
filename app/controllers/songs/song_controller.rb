c838382c8f721030d89ceff11586fa5ff128a8b3
punksguitar <hanahanaogutaro@gmail.com>
2025-10-05 01:16:50 +0900

    [add]楽曲再生APIに再生数カウント処理追加、ルーティング

--- app/controllers/songs/song_controller.rb
+++ app/controllers/songs/song_controller.rb
@@ -1,2 +1,13 @@
 class SongController < ApplicationController
+  def show
+    @song_id = params[:id]
+    @share_id = params[:share_id]
+
+    service = ShareSongService.new(song_id: @song_id, share_id: @share_id)
+
+    song = service.fetch_song_and_artist_name(@song_id)
+    render json: song
+
+    service.increment_shared_count
+  end
 end

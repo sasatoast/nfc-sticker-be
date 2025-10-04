b56500ed0c58d577a096c3877d0b4106c5594feb
punksguitar <hanahanaogutaro@gmail.com>
2025-10-04 15:15:34 +0900

    [add]songモデルとartistモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_03_053558) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_04_060941) do
+  create_table "artists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "picture_url"
+    t.string "sporify_url"
+    t.string "apple_url"
+    t.string "homepage_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "jwt_denylist", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "exp", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti", unique: true
+  end
+
+  create_table "songs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "source_url"
+    t.string "picture_url"
+    t.string "spotify_url"
+    t.string "apple_url"
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -33,4 +54,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_03_053558) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["share_id"], name: "index_users_on_share_id", unique: true
   end
+
+  add_foreign_key "songs", "artists"
 end

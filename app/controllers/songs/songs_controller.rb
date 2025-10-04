c10ad5e7bfe836fd2711690990dd2208e2b13a28
punksguitar <hanahanaogutaro@gmail.com>
2025-10-05 01:40:52 +0900

    [fix]seedに試験データ投入、ルーティング修正、artistにnameカラム追加

--- app/controllers/songs/songs_controller.rb
+++ app/controllers/songs/songs_controller.rb
@@ -1,11 +1,11 @@
-class SongController < ApplicationController
+class Songs::SongsController < ApplicationController
   def show
     @song_id = params[:id]
     @share_id = params[:share_id]
 
     service = ShareSongService.new(song_id: @song_id, share_id: @share_id)
 
-    song = service.fetch_song_and_artist_name(@song_id)
+    song = ShareSongService.fetch_song_and_artist_name(@song_id)
     render json: song
 
     service.increment_shared_count
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_04_152428) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_04_162151) do
   create_table "artists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "picture_url"
     t.string "sporify_url"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_04_152428) do
     t.string "homepage_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "jwt_denylist", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|

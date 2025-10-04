62e3da83dd7121e3c8e97c9ec7922df94deba382
punksguitar <hanahanaogutaro@gmail.com>
2025-10-04 14:11:46 +0900

    [add]認証チェック追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
     include RackSessionFix
+    skip_before_action :authenticate_user!, :only [:create]
     respond_to :json
     private
 

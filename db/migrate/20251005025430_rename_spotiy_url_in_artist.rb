273e5e059affecbc7182b45136eeea507db327ec
punksguitar <hanahanaogutaro@gmail.com>
2025-10-05 12:00:20 +0900

    [fix]rubocop

--- db/migrate/20251005025430_rename_spotiy_url_in_artist.rb
+++ db/migrate/20251005025430_rename_spotiy_url_in_artist.rb
@@ -1,6 +1,6 @@
 class RenameSpotiyUrlInArtist < ActiveRecord::Migration[8.0]
   def change
     rename_column :artists, :sporify_url, :spotify_url
-    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
+    # Ex:- rename_column("admin_users", "pasword","hashed_pasword")
   end
 end

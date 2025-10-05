9474f7aedab2718f793537d6b652cf9a3916d211
punksguitar <hanahanaogutaro@gmail.com>
2025-10-05 19:05:46 +0900

    [add]共有された楽曲取得API作成

---
+++ app/services/shared_songs_service.rb
@@ -0,0 +1,25 @@
+class SharedSongsService
+  include Callable
+  def initialize(user_id:)
+    @user_id = user_id
+  end
+
+  def call
+    song_data = fetch_shared_songs_by_user_id(@user_id)
+    song_data
+  end
+
+  def fetch_shared_songs_by_user_id(user_id)
+    shared_songs_ids = UsersSharedSong.where(user_id: user_id).pluck(:song_id)
+    songs_with_artist = Song.where(id: shared_songs_ids).includes(:artist)
+    song_data = songs_with_artist.map do |song|
+      {
+        song_id: song.id,
+        song_name: song.name,
+        song_picture_url: song.picture_url,
+        artist_name: song.artist.name
+      }
+    end
+    song_data
+  end
+end

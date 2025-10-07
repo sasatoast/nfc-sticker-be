7ebd11f74b6628bfda14e2c84bef6d7ca5b5d37d
punksguitar <hanahanaogutaro@gmail.com>
2025-10-07 14:56:11 +0900

    [fix]旧パスワード生成メソッド削除

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,4 @@
 class Song < ApplicationRecord
-  after_create { ShareSongService.create_password(self) }
   belongs_to :artist
   has_many :users_shared_songs
   has_many :shared_counts, dependent: :destroy

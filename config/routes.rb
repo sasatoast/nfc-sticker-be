e4f7e781f378a6fdd5eb49368b78ca0574ffe3d8
punksguitar <hanahanaogutaro@gmail.com>
2025-10-08 14:58:35 +0900

    add: API作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
         registrations: "users/registrations"
       }
       get "player/songs/:id", to: "songs/songs#show"
-      get "users/received/shared/songs", to: "songs/songs#show_shared_song"
+      get "users/received/songs", to: "songs/songs#show_received_song"
       post "users/songs/:song_id/unlock", to: "songs/songs#register_sharable_song"
       post "songs", to: "songs/songs#create_song"
       get "users/songs", to: "songs/songs#show_sharable_song"
+      get "users/shared/songs", to: "songs/songs#show_shared_song"
 end

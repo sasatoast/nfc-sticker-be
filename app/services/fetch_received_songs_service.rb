aa7b65757bef8ac94200ca1973883df2b2cdaae6
punksguitar <hanahanaogutaro@gmail.com>
2025-10-08 14:13:51 +0900

    fix: サービスクラス命名変更

--- app/services/fetch_received_songs_service.rb
+++ app/services/fetch_received_songs_service.rb
@@ -1,16 +1,16 @@
-class FetchSharedSongsService
+class FetchReceivedSongsService
   include Callable
   def initialize(user_id:)
     @user_id = user_id
   end
 
   def call
-    fetch_shared_songs_by_user_id(@user_id)
+    fetch_received_songs_by_user_id(@user_id)
   end
 
-  def fetch_shared_songs_by_user_id(user_id)
-    shared_songs_ids = UsersSharedSong.where(user_id: user_id).pluck(:song_id)
-    songs_with_artist = Song.where(id: shared_songs_ids).includes(:artist)
+  def fetch_received_songs_by_user_id(user_id)
+    received_songs_ids = UsersSharedSong.where(user_id: user_id).pluck(:song_id)
+    songs_with_artist = Song.where(id: received_songs_ids).includes(:artist)
     songs_with_artist.map do |song|
       {
         song_id: song.id,

27c6a32fbde0566a40e8994e745330cb11a0dd9d
punksguitar <hanahanaogutaro@gmail.com>
2025-10-08 14:58:35 +0900

    add: API作成

--- app/controllers/songs/songs_controller.rb
+++ app/controllers/songs/songs_controller.rb
@@ -1,6 +1,6 @@
 class Songs::SongsController < ApplicationController
   include Devise::Controllers::Helpers
-  before_action :authenticate_user!, only: [ :show_shared_song, :register_sharable_song, :show_sharable_song ]
+  before_action :authenticate_user!, only: [ :show_received_song, :register_sharable_song, :show_sharable_song, :show_shared_song]
   # あとでここにcreate_song追加
   def show
     song_data = PlaySongService.call(
@@ -15,7 +15,7 @@ class Songs::SongsController < ApplicationController
     end
   end
 
-  def show_shared_song
+  def show_received_song
     shared_song_data = FetchReceivedSongsService.call(
       user_id: current_user&.id
     )
@@ -65,6 +65,17 @@ class Songs::SongsController < ApplicationController
       render json: songs_data
     else
       render json: { "message": "共有できる曲はまだありません" }
+    end
+  end
+
+  def fetch_shared_song
+    songs_data = FetchSharedSongsService.call(
+      share_id: current_user.share_id
+    )
+    if songs_data.present?
+      render json: songs_data
+    else
+      render json: { "message": "まだ共有をしていません"}
     end
   end
 end

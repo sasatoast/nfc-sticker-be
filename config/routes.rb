120f1c2b05b21b1e6e6c4bbe69457fb80e54d21f
punksguitar <hanahanaogutaro@gmail.com>
2025-10-08 13:39:49 +0900

    [refactor]URI変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
         registrations: "users/registrations"
       }
       get "player/songs/:id", to: "songs/songs#show"
-      get "users/shared/songs", to: "songs/songs#show_shared_song"
+      get "users/received/shared/songs", to: "songs/songs#show_shared_song"
       post "users/songs/:song_id/unlock", to: "songs/songs#register_sharable_song"
       post "songs", to: "songs/songs#create_song"
       get "users/songs", to: "songs/songs#show_sharable_song"

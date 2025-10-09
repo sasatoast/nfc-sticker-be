9a1295b74b4c1963ceddecff77840820a6954eb8
punksguitar <hanahanaogutaro@gmail.com>
2025-10-09 14:46:15 +0900

    add: アーティスト情報取得APIの作成

---
+++ app/controllers/artists/artists_controller.rb
@@ -0,0 +1,15 @@
+class Artists::ArtistsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show_artist]
+
+  def show_artist
+    status, result = Artists::FetchArtistService.call(
+      artist_id: params[:artist_id]
+    )
+    case status
+    when :ok
+      render json: result
+    when :not_found
+      render json: { error: 'Artist not found' }, status: :not_found
+    end
+  end
+end

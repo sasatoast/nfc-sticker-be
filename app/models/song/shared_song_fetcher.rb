661ce95796682a84dc3751353239daccd84d1d74
punksguitar <hanahanaogutaro@gmail.com>
2025-10-09 20:19:13 +0900

    feat add replace service file

---
+++ app/models/song/shared_song_fetcher.rb
@@ -0,0 +1,25 @@
+class Song
+  class SharedSongFetcher
+    include Callable
+    def initialize(share_id:)
+      @share_id = share_id
+    end
+
+    def call
+      fetch_songs(@share_id)
+    end
+
+    def fetch_songs(share_id)
+      count_data = SharedCount.where(share_id: share_id).includes(song: :artist)
+      count_data.map do |data|
+        song = data.song
+        {
+          "name": song.name,
+          "artist_name": song.artist.name,
+          "count": data.count,
+          "song_id": song.id
+        }
+      end
+    end
+  end
+end

c6f2bca7a56a824f052f1c537071a4397ff27e18
punksguitar <hanahanaogutaro@gmail.com>
2025-10-09 20:20:24 +0900

    delete unnecessary old file

--- app/services/concerns/callable.rb
+++
@@ -1,9 +0,0 @@
-module Callable
-  extend ActiveSupport::Concern
-  # concernで切り出すか、applicationService作ってまとめるかは要検討
-  class_methods do
-    def call(*args, **kwargs, &block)
-      new(*args, **kwargs, &block).call
-    end
-  end
-end

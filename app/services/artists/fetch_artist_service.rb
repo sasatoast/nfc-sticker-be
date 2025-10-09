2e7a9f5f3501911f2d10021c37f82df8ca7280c9
punksguitar <hanahanaogutaro@gmail.com>
2025-10-09 14:46:15 +0900

    add: アーティスト情報取得APIの作成

---
+++ app/services/artists/fetch_artist_service.rb
@@ -0,0 +1,30 @@
+module Artists
+  class FetchArtistService
+    include Callable
+    def initialize(artist_id:)
+      @artist_id = artist_id
+    end
+
+    def call
+      fetch_artist(@artist_id)
+    end
+
+    def fetch_artist(artist_id)
+      artist = Artist.find_by(id: artist_id)
+      if artist.present?
+        [
+          :ok,
+          {
+            name: artist.name,
+            picture_url: artist.picture_url,
+            spotify_url: artist.spotify_url,
+            apple_url: artist.apple_url,
+            homepage_url: artist.homepage_url
+          }
+        ]
+      else
+        [:not_found, nil]
+      end
+    end
+  end
+end

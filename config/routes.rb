5d92abc2d3cbc748962397437a864b0b9a25a6e7
punksguitar <hanahanaogutaro@gmail.com>
2025-10-10 16:41:09 +0900

    refactor route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
       devise_for :users, path: "", path_names: {
         sign_in: "login",
         sign_out: "logout",
         registration: "signup"
       },
       controllers: {
-        sessions: "users/sessions",
-        registrations: "users/registrations"
+        sessions: "api/v1/users/sessions",
+        registrations: "api/v1/users/registrations"
       }
       get "player/songs/:id", to: "songs/songs#show"
       get "users/received/songs", to: "songs/songs#show_received_song"
@@ -16,4 +18,6 @@ Rails.application.routes.draw do
       get "users/shared/songs", to: "songs/songs#show_shared_song"
       get "ranking/:artist_id", to: "songs/songs#show_share_ranking_by_artist_id"
       get "artists/:artist_id", to: "artists/artists#show_artist"
+    end
+  end
 end

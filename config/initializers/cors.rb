8836fa763cab921292c663d43a215b10a804d659
sasatoast <hanahanaogutaro@gmail.com>
2025-10-13 15:05:53 +0900

    fix cors front access url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("CORS_ORIGINS", "localhost:3000")
+    origins ENV.fetch("CORS_ORIGINS", "localhost:3001")
     resource "*",
       headers: :any,
       methods:  %i[get post put patch delete options head],

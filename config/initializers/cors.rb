5cbbdaa658a5bb1e42e546646b956936ffe4afd8
sasatoast <hanahanaogutaro@gmail.com>
2025-10-14 14:25:17 +0900

    fix: CORS設定にhttpsプロトコルを追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("CORS_ORIGINS", "nfc-sticker.vercel.app")
+    origins ENV.fetch("CORS_ORIGINS", "https://nfc-sticker.vercel.app")
     resource "*",
       headers: :any,
       methods:  %i[get post put patch delete options head],

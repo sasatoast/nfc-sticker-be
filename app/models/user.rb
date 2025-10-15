8ea2d890a3e5c9029321587b70f2df0242e4341f
punksguitar <hanahanaogutaro@gmail.com>
2025-10-15 14:36:07 +0900

    fix: rename JwtDenylist.rb to jwt_denylist.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+         :jwt_authenticatable, jwt_revocation_strategy: :jwt_denylist
   before_create :generate_share_id
 
   has_many :users_shared_songs

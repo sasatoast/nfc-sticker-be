e99bd186afd8bf0ce9377ccb8b2b844cfa327730
punksguitar <hanahanaogutaro@gmail.com>
2025-10-26 17:37:54 +0900

    refactor add status notfound

--- app/models/artist/shared_artist_fetcher.rb
+++ app/models/artist/shared_artist_fetcher.rb
@@ -12,14 +12,19 @@ class Artist
     def fetch_shared_artist(user_id)
       shared_artists_ids = UsersSharedSong.where(user_id: user_id).pluck(:artist_id)
       artists = Artist.where(id: shared_artists_ids)
-      result = artists.map do |artist|
-        {
-          artist_id: artist.id,
-          artist_name: artist.name,
-          artist_picture_url: artist.picture_url
-        }
+      
+      if artists.empty?
+        [ :not_found, nil ]
+      else
+        result = artists.map do |artist|
+          {
+            artist_id: artist.id,
+            artist_name: artist.name,
+            artist_picture_url: artist.picture_url
+          }
+        end
+        [ :ok, result ]
       end
-      [ :ok, result ]
     end
   end
 end
